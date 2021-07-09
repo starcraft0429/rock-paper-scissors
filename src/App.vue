2a824d58ed565cd1e591d5e68a3e0299e9984cbd
webdeveloper0429 <webdeveloper0429@yahoo.com>
2021-07-09 19:55:30 -0400

    add rules page

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,17 @@
     <div class="d-flex flex-column h-100">
       <Header />
       <Main />
-      <footer class="">
-        <div class="button-w child-center">
-          <b-button variant="outline-secondary">RULES</b-button>
-        </div>
-      </footer>
+      <Footer />
     </div>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 import Main from "@/components/Main.vue";
 export default {
   name: "App",
-  components: { Header, Main },
+  components: { Header, Footer, Main },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer class="">
+    <div class="button-w child-center">
+      <b-button variant="outline-secondary" v-b-modal="'rules-modal'">
+        RULES
+      </b-button>
+    </div>
+    <b-modal id="rules-modal" centered hide-header hide-footer>
+      <div class="rule-modal-content">
+        <div
+          class="
+            w-100
+            d-flex
+            align-items-center
+            justify-content-center justify-content-xl-between
+          "
+        >
+          <h2 class="m-0 text-uppercase text-primary font-weight-bold">
+            Rules
+          </h2>
+          <b-button
+            class="close d-none d-xl-block"
+            @click="$bvModal.hide('rules-modal')"
+          >
+            <span>&times;</span>
+          </b-button>
+        </div>
+        <img
+          :src="require('@/assets/images/image-rules-bonus.svg')"
+          alt="rules"
+        />
+        <b-button
+          class="close d-none d-sm-block d-xl-none"
+          @click="$bvModal.hide('rules-modal')"
+        >
+          <span>&times;</span>
+        </b-button>
+      </div>
+    </b-modal>
+  </footer>
+</template>
