4e94afcc1734cb525e21538a068098c235db470f
webdeveloper0429 <webdeveloper0429@yahoo.com>
2021-07-09 14:50:53 -0400

    add bootstrap vue and init theme

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import { BootstrapVue } from "bootstrap-vue";
+
+import "@/assets/scss/app.scss";
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
